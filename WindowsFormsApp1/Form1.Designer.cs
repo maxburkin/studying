0921b53eb4a808fbf8d72ce67134d6927d0f0436
max-b <max-b@DESKTOP-QU11GMD>
2021-10-12 13:02:39 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.renameIt = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialog3 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // renameIt
+            // 
+            this.renameIt.AccessibleDescription = "";
+            this.renameIt.Location = new System.Drawing.Point(176, 338);
+            this.renameIt.Name = "renameIt";
+            this.renameIt.Size = new System.Drawing.Size(164, 47);
+            this.renameIt.TabIndex = 0;
+            this.renameIt.Text = "rename it!";
+            this.renameIt.UseVisualStyleBackColor = true;
+            this.renameIt.Click += new System.EventHandler(this.renameIt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.renameIt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button renameIt;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void renameIt_Click(object sender, EventArgs e)
+        {
+            //с помощью диалога выбираем папку с файлами
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+
+            Encoding win1252  = Encoding.GetEncoding(1252); 
+            Encoding win1251 = Encoding.GetEncoding(1251);
+
+            // ýëåêòðèôèêàöèÿ
+
+            if (result == DialogResult.OK)
+            {
+                //Получить все файлы в папке
+                string[] files = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+                int ind = 0;
+                for (int i = 0; i < files.Length; i++)
+                {
+                    //поиск индекса последнего слеша
+                    ind = files[i].LastIndexOf('\\');
+                    //переименование
+                    string FileName = Path.GetFileNameWithoutExtension(files[i]);
+
+                    byte[] win1252Bytes  = win1252.GetBytes(FileName);
+
+                    // ну попробуем - взлетит или всплывет
+                    byte[] win1251Bytes = Encoding.Convert(win1252, win1251, win1252Bytes);
+
+                    char[] win1251Chars = new char[win1251.GetCharCount(win1251Bytes, 0, win1251Bytes.Length)];
+                    win1251.GetChars(win1251Bytes, 0, win1251Bytes.Length, win1251Chars, 0);
+                    string win1251String = new string(win1251Chars);
+
+                    File.Move(files[i], files[i].Remove(ind + 1) + win1251String + ".txt");
+                }
+                MessageBox.Show("Готово!");
+            }
+        }
+    }
+}
